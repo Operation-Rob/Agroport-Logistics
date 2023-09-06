78f176383dd6d13381b8d62c02737836bb6851b7
cjnbennett <conor.jn.bennett@gmail.com>
2023-09-06 16:31:00 +0800

    mapbox

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <MapboxComponent />
   </div>
 </template>
+
+<script lang="ts">
+import MapboxComponent from '../components/Mapbox.vue'
+
+export default {
+  components: {
+      MapboxComponent
+  }
+}
+</script>
---
+++ components/Mapbox.vue
@@ -0,0 +1,37 @@
+<template>
+    <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from 'vue';
+import mapboxgl from 'mapbox-gl';
+
+export default {
+name: 'MapboxComponent',
+setup() {
+    const mapContainer = ref(null);
+    let map: mapboxgl.Map | null = null;
+
+    onMounted(() => {
+        const runtimeConfig = useRuntimeConfig();
+        mapboxgl.accessToken = runtimeConfig.public.mapboxToken;
+
+    map = new mapboxgl.Map({
+        container: mapContainer.value as HTMLElement,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [0, 0],  // starting position [lng, lat]
+        zoom: 9
+    });
+    });
+
+    return { mapContainer };
+}
+}
+</script>
+
+<style>
+.map-container {
+width: 100%;
+height: 400px; /* You can adjust as needed */
+}
+</style>  
