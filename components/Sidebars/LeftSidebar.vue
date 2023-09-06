6b04a10e10834ba6ac626644e933eabb22fc7765
cjnbennett <conor.jn.bennett@gmail.com>
2023-09-07 01:26:03 +0800

    hooked ship table into ship store

--- components/Sidebars/LeftSidebar.vue
+++ components/Sidebars/LeftSidebar.vue
@@ -2,23 +2,27 @@
     <div class="fixed top-0 left-0 w-1/5 h-screen bg-black p-4 z-10 text-white">
         <h1 class="font-bold underline pb-2">Ship Table</h1>
         <ul class="space-y-3">
-            <ShipCard v-for="ship in filteredShips" :key="ship.name" :shipName="ship.name" :shipType="ship.type" :cargo="ship.cargo" :eta="ship.eta" :volume="ship.volume" />
+            <ShipCard v-for="ship in filteredShips" :key="ship.properties['Vessel Name']" :shipName="ship.properties['Vessel Name']" :shipType="ship.properties['Vessel Type - Detailed']" :cargo="ship.properties['Product']" :eta="ship.properties['Calculated Eta']" :volume="(ship.properties['Capacity - Gt'].toString())" />
         </ul>
     </div>
 </template>
 
 <script lang="ts">
+import { useShipsStore } from '@/stores/shipsStore';
 import ShipCard from './ShipCard.vue';
+
     export default {
         name: 'LeftSidebar',
         components: {
         ShipCard
     },
-        data() {
-            return {
-                filteredShips: [{ name: 'Ship 1', type: 'Bulk Carrier', cargo: 'Ammonia', eta: '08/09/2023 12:35', volume: '5MT' },{ name: 'Ship 2', type: 'Bulk Carrier', cargo: 'Ammonia', eta: '08/09/2023 12:35', volume: '10MT' }]
-            }
-        }
+    setup() {
+        const shipsStore = useShipsStore();
+        const filteredShips = computed(() => shipsStore.filteredShips);
+        return {
+            filteredShips
+        };
+    }        
     }
 </script>
 
