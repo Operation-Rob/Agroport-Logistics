e2a7518fac03affa375d03cf72bc7f6eda172173
Michael Nef <michael@nef.net.au>
2023-09-07 00:21:11 +0800

    pins update when store updates

--- components/Mapbox/Mapbox.vue
+++ components/Mapbox/Mapbox.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted, watch } from 'vue';
 import mapboxgl from 'mapbox-gl';
 import { usePortsStore } from '@/stores/portsStore';
 import { useShipsStore } from '@/stores/shipsStore';
@@ -15,8 +15,32 @@ export default {
         const mapContainer = ref(null);
         let map: mapboxgl.Map | null = null;
 
+        let portMarkers: mapboxgl.Marker[] = [];
+        let shipMarkers: mapboxgl.Marker[] = [];
+
         const portsStore = usePortsStore();
         const shipsStore = useShipsStore();
+
+        const addMarkers = (items: any[], markersArray: mapboxgl.Marker[], color?: string) => {
+            for (const item of items) {
+                const [longitude, latitude] = item.geometry.coordinates;
+                const options: mapboxgl.MarkerOptions = { rotationAlignment: 'horizon' };
+                if (color) options.color = color;
+
+                const marker = new mapboxgl.Marker(options)
+                    .setLngLat([longitude, latitude])
+                    .addTo(map!);
+
+                markersArray.push(marker);
+            }
+        };
+
+        const clearMarkers = (markersArray: mapboxgl.Marker[]) => {
+            for (const marker of markersArray) {
+                marker.remove();
+            }
+            markersArray.length = 0;
+        };
 
         onMounted(() => {
             const runtimeConfig = useRuntimeConfig();
@@ -30,24 +54,26 @@ export default {
             });
 
             map.on('load', () => {
-                for (const port of portsStore.filteredPorts) {
-                    const [longitude, latitude] = port.geometry.coordinates;
-                    const marker = new mapboxgl.Marker({
-                            rotationAlignment: 'horizon',
-                            color: '#FF0000'
-                        }).setLngLat([longitude, latitude])
-                          .addTo(map);
-                }
+                addMarkers(portsStore.filteredPorts, portMarkers, '#FF0000');
+                addMarkers(shipsStore.filteredShips, shipMarkers);
+            });
 
-                for (const ship of shipsStore.filteredShips) {
-                    const [longitude, latitude] = ship.geometry.coordinates;
-                    const marker = new mapboxgl.Marker({
-                            rotationAlignment: 'horizon',
-                        }).setLngLat([longitude, latitude])
-                          .addTo(map);
+            watch(portsStore.filteredPorts, (newPorts) => {
+                clearMarkers(portMarkers);
+                addMarkers(newPorts, portMarkers, '#FF0000');
+            });
+
+            watch(shipsStore.filteredShips, (newShips) => {
+                clearMarkers(shipMarkers);
+                addMarkers(newShips, shipMarkers);
+            });
+
+            onUnmounted(() => {
+                clearMarkers([...portMarkers, ...shipMarkers]);
+                if (map) {
+                    map.remove();
                 }
             });
-            
         });
 
         return { mapContainer };
