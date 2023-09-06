11e502948d9433ac1b4d0910a98b28ecd3b61c0c
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-06 20:35:18 +0800

    change right sidebar width

--- app.vue
+++ app.vue
@@ -3,18 +3,19 @@
     <MapboxComponent class="w-full h-screen" />
   </main>
   <LeftSidebar />
+  <RightSidebar />
 </template>
 
 <script lang="ts">
-import MapboxComponent from '../components/Mapbox/Mapbox.vue'
-import LeftSidebar from '../components/Sidebars/LeftSidebar.vue'
-import RightSidebar from '../components/Sidebars/RightSidebar.vue'
+import MapboxComponent from "../components/Mapbox/Mapbox.vue";
+import LeftSidebar from "../components/Sidebars/LeftSidebar.vue";
+import RightSidebar from "../components/Sidebars/RightSidebar.vue";
 
 export default {
   components: {
     MapboxComponent,
     LeftSidebar,
-    RightSidebar
-  }
-}
+    RightSidebar,
+  },
+};
 </script>
--- components/Sidebars/RightSidebar.vue
+++ components/Sidebars/RightSidebar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="fixed top-0 right-0 w-64 h-screen bg-gray-200 p-4 z-10">
-        Right Sidebar Content
-    </div>
+  <div class="fixed top-0 right-0 w-1/5 h-screen bg-gray-200 p-4 z-10">
+    Right Sidebar Content
+  </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'RightSidebar'
-    }
+export default {
+  name: "RightSidebar",
+};
 </script>
