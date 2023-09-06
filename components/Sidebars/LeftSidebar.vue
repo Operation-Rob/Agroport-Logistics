20b4a7d2d2cc8121a66dfef57b523e4f920d9a7f
cjnbennett <conor.jn.bennett@gmail.com>
2023-09-07 00:58:17 +0800

    shipcard styling

--- components/Sidebars/LeftSidebar.vue
+++ components/Sidebars/LeftSidebar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="fixed top-0 left-0 w-1/5 h-screen bg-black p-4 z-10 text-white">
-        <h1>Ship Table</h1>
-        <ul>
-            <ShipCard v-for="ship in filteredShips" :key="ship.name" :shipName="ship.name" />
+        <h1 class="font-bold underline pb-2">Ship Table</h1>
+        <ul class="space-y-3">
+            <ShipCard v-for="ship in filteredShips" :key="ship.name" :shipName="ship.name" :shipType="ship.type" :cargo="ship.cargo" :eta="ship.eta" :volume="ship.volume" />
         </ul>
     </div>
 </template>
@@ -16,7 +16,7 @@ import ShipCard from './ShipCard.vue';
     },
         data() {
             return {
-                filteredShips: [{ name: 'Ship 1' },{ name: 'Ship 2' }]
+                filteredShips: [{ name: 'Ship 1', type: 'Bulk Carrier', cargo: 'Ammonia', eta: '08/09/2023 12:35', volume: '5MT' },{ name: 'Ship 2', type: 'Bulk Carrier', cargo: 'Ammonia', eta: '08/09/2023 12:35', volume: '10MT' }]
             }
         }
     }
--- components/Sidebars/ShipCard.vue
+++ components/Sidebars/ShipCard.vue
@@ -1,5 +1,13 @@
 <template>
-<li>{{ shipName }}</li>
+<li class="outline rounded-md bg-gray-950">
+    <div class="p-2 pt-1">
+        <div>{{ shipName }}</div>
+        <div class="italic text-sm">{{ shipType }}</div>
+        <div class="text-sm">ETA: {{ eta }}</div>
+        <div class="text-sm">Cargo: {{ cargo }}</div>
+        <div class="text-sm">Volume: {{ volume }}</div>
+    </div>
+</li>
 </template>
 
 <script lang="ts">
@@ -7,8 +15,24 @@ export default {
 name: 'ShipCard',
 props: {
     shipName: {
-    type: String,
-    required: true
+        type: String,
+        required: true
+    },
+    shipType: {
+        type: String,
+        required: true
+    },
+    eta: {
+        type: String,
+        required: true
+    },
+    cargo: {
+        type: String,
+        required: true
+    },
+    volume: {
+        type: String,
+        required: true
     }
 }
 }
