989161915a7e4e5db0a40b8b357cd14f7955a0d8
Michael Nef <michael@nef.net.au>
2023-09-07 00:12:18 +0800

    added basic pins

--- components/Mapbox/Mapbox.vue
+++ components/Mapbox/Mapbox.vue
@@ -5,27 +5,53 @@
 <script lang="ts">
 import { ref, onMounted } from 'vue';
 import mapboxgl from 'mapbox-gl';
+import { usePortsStore } from '@/stores/portsStore';
+import { useShipsStore } from '@/stores/shipsStore';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default {
-name: 'MapboxComponent',
-setup() {
-    const mapContainer = ref(null);
-    let map: mapboxgl.Map | null = null;
+    name: 'MapboxComponent',
+    setup() {
+        const mapContainer = ref(null);
+        let map: mapboxgl.Map | null = null;
 
-    onMounted(() => {
-        const runtimeConfig = useRuntimeConfig();
-        mapboxgl.accessToken = runtimeConfig.public.mapboxToken;
+        const portsStore = usePortsStore();
+        const shipsStore = useShipsStore();
 
-    map = new mapboxgl.Map({
-        container: mapContainer.value as HTMLElement,
-        style: 'mapbox://styles/mapbox/outdoors-v12',
-        center: [0, 0],  // starting position [lng, lat]
-        zoom: 9
-    });
-    });
+        onMounted(() => {
+            const runtimeConfig = useRuntimeConfig();
+            mapboxgl.accessToken = runtimeConfig.public.mapboxToken;
 
-    return { mapContainer };
-}
+            map = new mapboxgl.Map({
+                container: (mapContainer.value as any) as HTMLElement,
+                style: 'mapbox://styles/mapbox/outdoors-v12',
+                center: [0, 0],  // starting position [lng, lat]
+                zoom: 9
+            });
+
+            map.on('load', () => {
+                for (const port of portsStore.filteredPorts) {
+                    const [longitude, latitude] = port.geometry.coordinates;
+                    const marker = new mapboxgl.Marker({
+                            rotationAlignment: 'horizon',
+                            color: '#FF0000'
+                        }).setLngLat([longitude, latitude])
+                          .addTo(map);
+                }
+
+                for (const ship of shipsStore.filteredShips) {
+                    const [longitude, latitude] = ship.geometry.coordinates;
+                    const marker = new mapboxgl.Marker({
+                            rotationAlignment: 'horizon',
+                        }).setLngLat([longitude, latitude])
+                          .addTo(map);
+                }
+            });
+            
+        });
+
+        return { mapContainer };
+    }
 }
 </script>
 
