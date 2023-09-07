9c30269f1bff63832c30001394b01490dac75dab
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-07 16:54:03 +0800

    header icon resize

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="flex items-center justify-center bg-black">
-      <img :src="agroImage" alt="Agro Image" class="w-auto h-12" />
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import agroImage from '~/assets/agro2.png';
-  
-  export default {
-    data() {
-      return {
-        agroImage
-      };
-    },
-    name: 'HeaderComponent'
-  };
-  </script>
-  
+  <div class="flex items-center justify-center bg-black">
+    <img :src="agroImage" alt="Agro Image" class="w-auto h-7 md:h-12" />
+  </div>
+</template>
+
+<script lang="ts">
+import agroImage from "~/assets/agro2.png";
+
+export default {
+  data() {
+    return {
+      agroImage,
+    };
+  },
+  name: "HeaderComponent",
+};
+</script>
