aff291ee883f453e8cd9669abddf3df1a9a98a4d
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-07 14:02:12 +0800

    formatting

--- components/Sidebars/LeftSidebar.vue
+++ components/Sidebars/LeftSidebar.vue
@@ -1,44 +1,47 @@
 <template>
-    <div class="fixed top-0 left-0 w-1/5 h-screen bg-black pr-0 py-2 pl-2 z-10 text-white">
-        <h1 class="font-bold underline pb-2 pl-2 text-lg">Ship Table</h1>
-        <div class="h-[calc(100vh-76px)] overflow-y-scroll py-2 pl-2 pr-4">
-            <ul class="space-y-3">
-                <ShipCard v-for="ship in filteredShips"
-                    :key="ship.properties['Vessel Name']"
-                    @ship-selected="handleShipSelected"
-                    :shipName="ship.properties['Vessel Name']"
-                    :shipType="ship.properties['Vessel Type - Detailed']"
-                    :cargo="ship.properties['Product']"
-                    :eta="ship.properties['Calculated Eta']"
-                    :volume="(ship.properties['Capacity - Gt'].toString())"
-                />
-            </ul>
-        </div>
+  <div
+    class="fixed top-0 left-0 w-1/5 h-screen bg-black pr-0 py-2 pl-2 z-10 text-white"
+  >
+    <h1 class="font-bold underline pb-2 pl-2 text-lg">Ship Table</h1>
+    <div class="h-[calc(100vh-76px)] overflow-y-scroll py-2 pl-2 pr-4">
+      <ul class="space-y-3">
+        <ShipCard
+          v-for="ship in filteredShips"
+          :key="ship.properties['Vessel Name']"
+          @ship-selected="handleShipSelected"
+          :shipName="ship.properties['Vessel Name']"
+          :shipType="ship.properties['Vessel Type - Detailed']"
+          :cargo="ship.properties['Product']"
+          :eta="ship.properties['Calculated Eta']"
+          :volume="ship.properties['Capacity - Gt'].toString()"
+        />
+      </ul>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { useShipsStore } from '@/stores/shipsStore';
-import ShipCard from './ShipCard.vue';
+import { useShipsStore } from "@/stores/shipsStore";
+import ShipCard from "./ShipCard.vue";
 
-    export default {
-        name: 'LeftSidebar',
-        components: {
-        ShipCard
-    },
-    setup(props, { emit }) {
-        const shipsStore = useShipsStore();
-        const filteredShips = computed(() => shipsStore.filteredShips);
-        const handleShipSelected = (shipName: string) => {
-            emit('focus-on-ship', shipName);
-        };
+export default {
+  name: "LeftSidebar",
+  components: {
+    ShipCard,
+  },
+  setup(props, { emit }) {
+    const shipsStore = useShipsStore();
+    const filteredShips = computed(() => shipsStore.filteredShips);
+    const handleShipSelected = (shipName: string) => {
+      emit("focus-on-ship", shipName);
+    };
 
-        return {
-            filteredShips,
-            handleShipSelected
-        };
-    }        
-    }
+    return {
+      filteredShips,
+      handleShipSelected,
+    };
+  },
+};
 </script>
 
 <style></style>
