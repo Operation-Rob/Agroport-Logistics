3b062f61a127ab78df47d682284d5d7ef64c207b
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-07 18:31:00 +0800

    responsive design for left sidebar

--- app.vue
+++ app.vue
@@ -1,35 +1,71 @@
-
 <template>
   <div class="h-screen flex flex-col overflow-hidden">
-    <Header class="h-[10%]" />
+    <Header
+      @left="
+        () => {
+          leftSidebarOpen = true;
+        }
+      "
+      @right="() => {}"
+      class="h-[10%]"
+    />
+
     <main class="h-[90%] overflow-y-hidden">
-      <LeftSidebar @focus-on-ship="focusOnShip" />
-      <RightSidebar />
+      <LeftSidebar
+        class="invisible md:visible transition"
+        @focus-on-ship="focusOnShip"
+      />
+
+      <transition
+        enter-from-class="opacity-0"
+        leave-to-class="opacity-0"
+        enter-active-class="transition duration-200"
+        leave-active-class="transition duration-200"
+      >
+        <div
+          v-if="leftSidebarOpen"
+          class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-stone-200 bg-opacity-10 backdrop-blur-sm transition"
+        >
+          <div
+            v-on-click-outside="
+              () => {
+                leftSidebarOpen = false;
+              }
+            "
+            :class="`bg-white rounded-2xl ${size}`"
+          >
+            <LeftSidebar @focus-on-ship="focusOnShip" />
+          </div>
+        </div>
+      </transition>
+      <RightSidebar class="invisible md:visible" />
       <MapboxComponent ref="mapboxRef" class="w-full h-screen" />
     </main>
     <Footer class="h-[5%] z-20" />
   </div>
 </template>
 
-<script lang="ts">
-import MapboxComponent from '../components/Mapbox/Mapbox.vue'
-import LeftSidebar from '../components/Sidebars/LeftSidebar.vue'
-import RightSidebar from '../components/Sidebars/RightSidebar.vue'
-import Header from '../components/Header/Header.vue'
-import Footer from '../components/Footer/Footer.vue'
+<script setup>
+import MapboxComponent from "../components/Mapbox/Mapbox.vue";
+import LeftSidebar from "../components/Sidebars/LeftSidebar.vue";
+import RightSidebar from "../components/Sidebars/RightSidebar.vue";
+import Header from "../components/Header/Header.vue";
+import Footer from "../components/Footer/Footer.vue";
+import { vOnClickOutside } from "@vueuse/components";
+const leftSidebarOpen = ref(false);
 
-export default {
-  components: {
-    MapboxComponent,
-    LeftSidebar,
-    RightSidebar,
-    Header,
-    Footer
-  },
-  methods: {
-    focusOnShip(shipName: string) {
-      this.$refs.mapboxRef.zoomToShip(shipName);
-    }
-  }
-}
+// export default {
+//   components: {
+//     MapboxComponent,
+//     LeftSidebar,
+//     RightSidebar,
+//     Header,
+//     Footer,
+//   },
+//   methods: {
+//     focusOnShip(shipName: string) {
+//       this.$refs.mapboxRef.zoomToShip(shipName);
+//     },
+//   },
+// };
 </script>
