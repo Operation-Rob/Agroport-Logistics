5c560c8fc40cc423c30fb081b2a11800cdae3bff
cjnbennett <conor.jn.bennett@gmail.com>
2023-09-07 16:24:06 +0800

    filters improvement

--- components/Sidebars/RightSidebar.vue
+++ components/Sidebars/RightSidebar.vue
@@ -1,24 +1,72 @@
 <template>
-    <div class="fixed top-0 right-0 w-1/5 h-screen bg-black pr-0 py-2 pl-2 z-10 text-white">
+    <div class="fixed top-0 right-0 w-1/5 h-screen bg-black pl-2 py-2 pr-1 z-10 text-white">
+        <div class="outline rounded-md bg-gray-950 p-2 pt-1">
         <h1 class="font-bold underline pb-2 pl-2 text-lg">Filters</h1>
         <div class="space-y-1">
-        <div>
+        <div class="text-sm">
             Cargo:
-            <select v-model="product" class="bg-gray-200 text-black px-1 rounded absolute right-2 border-2 border-gray-600 w-36">
+            <select v-model="product" class="bg-gray-200 text-black text-sm px-1 rounded absolute right-2 border-2 border-gray-600 w-36">
                 <option>All</option>
                 <option>Urea</option>
-                <option>Fertiliser 2</option>
-                <option>Fertiliser 3</option>
+                <option>UAN</option>
+                <option>MOP</option>
             </select>
         </div>
-        <div>
-            Country:
-            <select v-model="country" class="bg-gray-200 text-black px-1 rounded absolute right-2 border-2 border-gray-600 w-36">
+        <div class="text-sm">
+            Origin Country:
+            <select v-model="originCountry" class="bg-gray-200 text-black text-sm px-1 rounded absolute right-2 border-2 border-gray-600 w-36">
                 <option>All</option>
+                <option>Russia</option>
+                <option>Qatar</option>
+                <option>Saudi Arabia</option>
+                <option>Bahrain</option>
+                <option>Trinidad & Tobago</option>
                 <option>Indonesia</option>
-                <option>China</option>
-                <option>South Korea</option>
+                <option>Malaysia</option>
+                <option>Jordan</option>
             </select>
+        </div>
+        <div class="text-sm">
+            Dest. Country:
+            <select v-model="destCountry" class="bg-gray-200 text-black px-1 text-sm rounded absolute right-2 border-2 border-gray-600 w-36">
+                <option>All</option>
+                <option>Australia</option>
+                <option>Turkey</option>
+                <option>Spain</option>
+                <option>Israel</option>
+                <option>Libya</option>
+                <option>Bangladesh</option>
+                <option>Algeria</option>
+                <option>China</option>
+                <option>United Arab Emirates</option>
+                <option>South Africa</option>
+                <option>Brazil</option>
+                <option>Indonesia</option>
+                <option>Egypt</option>
+                <option>United States of America</option>
+                <option>Tunisia</option>
+                <option>Mexico</option>
+                <option>Greece</option>
+                <option>Russia</option>
+                <option>Pakistan</option>
+                <option>Singapore</option>
+                <option>Saudi Arabia</option>
+                <option>Yemen</option>
+                <option>Oman</option>
+                <option>Iraq</option>
+                <option>Kenya</option>
+                <option>Venezuela</option>
+                <option>Tanzania</option>
+                <option>Italy</option>
+                <option>Trinidad & Tobago</option>
+                <option>Chile</option>
+                <option>Phillipines</option>
+                <option>Japan</option>
+                <option>Thailand</option>
+                <option>India</option>
+                <option>Canada</option>
+            </select>
+        </div>
         </div>
         </div>
         <button class="bg-al-green hover:bg-[#6ff283] active:bg-[#4fa156] px-2 rounded text-black font-bold absolute right-3 bottom-[40px]">Export</button>
@@ -39,23 +87,60 @@ type ProductMapping = {
 
 const countryCodeMapping: CountryCodeMapping = {
     "All": null,
-    "Indonesia": "IN",
+    "Russia": "RU",
+    "Qatar": "QA",
+    "Bahrain": "BH",
+    "Trinidad & Tobago": "TT",
+    "Indonesia": "ID",
+    "Malaysia": "MY",
+    "Jordan": "JO",
+    "Australia": "AU",
+    "Turkey": "TR",
+    "Spain": "ES",
+    "Israel": "IL",
+    "Libya": "LY",
+    "Bangladesh": "BD",
+    "Algeria": "DZ",
     "China": "CN",
-    "South Korea": "KR", // not sure about this one, but maybe we just don't have any data for it?
+    "United Arab Emirates": "AE",
+    "South Africa": "ZA",
+    "Brazil": "BR",
+    "Egypt": "EG",
+    "United States of America": "US",
+    "Tunisia": "TN",
+    "Mexico": "MX",
+    "Greece": "GR",
+    "Pakistan": "PK",
+    "Singapore": "SG",
+    "Saudi Arabia": "SA",
+    "Yemen": "YE",
+    "Oman": "OM",
+    "Iraq": "IQ",
+    "Kenya": "KE",
+    "Venezuela": "VE",
+    "Tanzania": "TZ",
+    "Italy": "IT",
+    "Chile": "CL",
+    "Phillipines": "PH",
+    "Japan": "JP",
+    "Thailand": "TH",
+    "India": "ID",
+    "Canada": "CA"
 };
 
 const productMapping: ProductMapping = {
     "All": null,
     "Urea": "Urea",
-    "Fertiliser 2": "FERTILISER2",
-    "Fertiliser 3": "FERTILISER3",
+    "UAN": "UAN",
+    "MOP": "MOP",
 };
 
 export default {
     name: 'RightSidebar',
     setup() {
         const product = ref('All');
-        const country = ref('All');
+        const originCountry = ref('All');
+        const destCountry = ref('All');
         const shipsStore = useShipsStore();
         
         watch(product, (newCargoValue) => {
@@ -63,14 +148,20 @@ export default {
             shipsStore.setFilters('product', mappedProduct);
         });
 
-        watch(country, (newCountryValue) => {
-            const countryCode = countryCodeMapping[newCountryValue];
+        watch(originCountry, (newOriginCountryValue) => {
+            const countryCode = countryCodeMapping[newOriginCountryValue];
             shipsStore.setFilters('origin', countryCode);
         });
 
+        watch(destCountry, (newDestCountryValue) => {
+            const countryCode = countryCodeMapping[newDestCountryValue];
+            shipsStore.setFilters('dest', countryCode);
+        })
+
         return {
             product,
-            country
+            originCountry,
+            destCountry
         };
     }
 }
