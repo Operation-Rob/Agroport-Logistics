9329543f7f53cf6e513a8d58ce0ee43846563322
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-07 17:31:22 +0800

    remove left bar styling

--- components/Sidebars/LeftSidebar.vue
+++ components/Sidebars/LeftSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-0 invisible md:visible left-0 w-1/5 h-screen bg-black pr-0 py-2 pl-2 z-10 text-white"
+    class="fixed top-0 left-0 w-1/5 h-screen bg-black pr-0 py-2 pl-2 z-10 text-white"
   >
     <h1 class="font-bold underline pb-2 pl-2 text-lg">Ship Table</h1>
     <div class="h-[calc(100vh-76px)] overflow-y-scroll py-2 pl-2 pr-4">
