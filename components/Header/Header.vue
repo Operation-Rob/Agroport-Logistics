e587c91f5038da3aeb2a2927c0fe65d85c579c8f
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-07 18:22:53 +0800

    add emits to header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="flex items-center justify-center bg-black">
+    <div
+      class="h-5 w-5 bg-white cursor-pointer"
+      @click="
+        () => {
+          emit('left');
+        }
+      "
+    >
+      Open left
+    </div>
     <img :src="agroImage" alt="Agro Image" class="w-auto h-7 md:h-12" />
+    <div
+      class="h-5 w-5 bg-white cursor-pointer"
+      @click="
+        () => {
+          emit('right');
+        }
+      "
+    >
+      R
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import agroImage from "~/assets/agro2.png";
-
-export default {
-  data() {
-    return {
-      agroImage,
-    };
-  },
-  name: "HeaderComponent",
-};
+const emit = defineEmits(["left","right"]);
 </script>
